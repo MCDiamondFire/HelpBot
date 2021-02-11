8ae2eb0185ac03380a904380c26b763389d9a97f
Reason <tylerwalker05@hotmail.co.uk>
2021-02-11 12:33:51 +0000

    Fix stuff

--- src/main/java/com/diamondfire/helpbot/util/DateUtil.java
+++ src/main/java/com/diamondfire/helpbot/util/DateUtil.java
@@ -11,26 +11,26 @@ public class DateUtil {
     public static Date toDate(java.sql.Date sqlDate) {
         return new Date(sqlDate.getDay(), sqlDate.getMonth(), sqlDate.getYear());
     }
-
+    
     public static Date toDate(long milli) {
         return new Date(milli);
     }
-
+    
     public static Date toDate(LocalDate date) {
         Calendar calendar = Calendar.getInstance();
         calendar.set(date.getYear(), date.getMonthValue() - 1, date.getDayOfMonth());
 
         return calendar.getTime();
     }
-
+    
     public static java.sql.Date toSqlDate(Date date) {
         return new java.sql.Date(date.toInstant().toEpochMilli());
     }
-
+    
     public static java.sql.Date toSqlDate(Instant instant) {
         return new java.sql.Date(instant.toEpochMilli());
     }
-
+    
     public static Timestamp toTimeStamp(Date date) {
         return new Timestamp(date.getTime());
     }
