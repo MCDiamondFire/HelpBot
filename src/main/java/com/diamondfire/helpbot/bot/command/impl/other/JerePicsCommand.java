6f5073e211ef42e21ac74207c3e0e9035f77a32c
ProjectDF <61924259+ProjectDF@users.noreply.github.com>
2021-02-12 10:44:23 -0600

    Update JerePicsCommand.java

--- src/main/java/com/diamondfire/helpbot/bot/command/impl/other/JerePicsCommand.java
+++ src/main/java/com/diamondfire/helpbot/bot/command/impl/other/JerePicsCommand.java
@@ -54,7 +54,9 @@ public class JerePicsCommand extends Command {
             "https://imgur.com/3yaNUzm.png",
             "https://cdn.discordapp.com/attachments/180793115223916544/539188769480507426/unknown.png",
             "https://cdn.discordapp.com/attachments/789266995672907818/809169563543273512/jeremaster.png",
-            "https://external-content.duckduckgo.com/iu/?reload=1613077331588&u=https://mc-heads.net/body/jeremaster/180.png"
+            "https://external-content.duckduckgo.com/iu/?reload=1613077331588&u=https://mc-heads.net/body/jeremaster/180.png",
+            "https://imgur.com/LFldK2p.png",
+            "https://imgur.com/aEq7voY.png"
     };
 
 
