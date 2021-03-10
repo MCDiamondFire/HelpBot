45d36326938bd389d867a0ecb5c113d44703c98e
Ymerald <Ymerald@gmail.com>
2021-03-09 18:14:13 -0800

    High res samquotes

--- src/main/java/com/diamondfire/helpbot/bot/command/impl/other/SamQuotesCommand.java
+++ src/main/java/com/diamondfire/helpbot/bot/command/impl/other/SamQuotesCommand.java
@@ -50,7 +50,9 @@ public class SamQuotesCommand extends Command {
             
             String[] message = event.getMessage().getContentRaw().split("/");
             
-            if(message.length != 6 || message[5] == null || message[6] == null || message[5].length() != 18 || message[6].length() != 18) {
+            System.out.println(message.length);
+            
+            if(message.length != 7 || message[5] == null || message[6] == null || message[5].length() != 18 || message[6].length() != 18) {
     
                 PresetBuilder error = new PresetBuilder();
                 
@@ -75,39 +77,39 @@ public class SamQuotesCommand extends Command {
                         BufferedImage samPfp = ImageIO.read(Objects.requireNonNull(IOUtil.getFileFromSite(samProfilePic, "sampfp.png")));
                         
                         String text = "             " + messageText.getContentRaw().replaceAll("[^a-zA-Z0-9 ]", "");
-                        
-                        int pfpWidth = samPfp.getWidth();
-                        
+    
+                        int pfpWidth = samPfp.getWidth() * 10;
+    
                         //crop pfp into a circle
-                        
+    
                         BufferedImage circleBuffer = new BufferedImage(pfpWidth, pfpWidth, BufferedImage.TYPE_INT_ARGB);
-                        
+    
                         Graphics2D g2 = circleBuffer.createGraphics();
-                        
+    
                         g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
-                        
+    
                         g2.setClip(new Ellipse2D.Float(0, 0, pfpWidth, pfpWidth));
                         g2.drawImage(samPfp, 0, 0, pfpWidth, pfpWidth, null);
-                        
+    
                         //convert text into image
-                        
+    
                         BufferedImage img = new BufferedImage(1, 1, BufferedImage.TYPE_INT_ARGB);
-                        
+    
                         Graphics2D g2d = img.createGraphics();
-                        
-                        Font font = new Font("Whitney", Font.PLAIN, 14);
+    
+                        Font font = new Font("Whitney", Font.PLAIN, 140);
                         g2d.setFont(font);
                         FontMetrics fm = g2d.getFontMetrics();
-                        
+    
                         int width = fm.stringWidth(text);
                         int height = fm.getHeight() * 2;
-                        
+    
                         g2d.dispose();
-                        
+    
                         img = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
-                        
+    
                         g2d = img.createGraphics();
-                        
+    
                         g2d.setRenderingHint(RenderingHints.KEY_ALPHA_INTERPOLATION, RenderingHints.VALUE_ALPHA_INTERPOLATION_QUALITY);
                         g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
                         g2d.setRenderingHint(RenderingHints.KEY_COLOR_RENDERING, RenderingHints.VALUE_COLOR_RENDER_QUALITY);
@@ -116,32 +118,32 @@ public class SamQuotesCommand extends Command {
                         g2d.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
                         g2d.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
                         g2d.setRenderingHint(RenderingHints.KEY_STROKE_CONTROL, RenderingHints.VALUE_STROKE_PURE);
-                        
+    
                         g2d.setFont(font);
                         g2d.setColor(Color.WHITE);
                         g2d.drawString(text, 0, fm.getAscent() * 2.2f);
-                        
+    
                         g2d.dispose();
-                        
+    
                         //convert SamMan_ into image
-                        
+    
                         BufferedImage img2 = new BufferedImage(1, 1, BufferedImage.TYPE_INT_ARGB);
-                        
+    
                         Graphics2D g2d2 = img2.createGraphics();
-                        
-                        Font font2 = new Font("Whitney", Font.BOLD, 14);
+    
+                        Font font2 = new Font("Whitney", Font.BOLD, 140);
                         g2d2.setFont(font2);
-                        FontMetrics fm2 = g2d.getFontMetrics();
-                        
-                        int width2 = fm2.stringWidth("             SamMan_");
+                        FontMetrics fm2 = g2d2.getFontMetrics();
+    
+                        int width2 = fm2.stringWidth("           SamMan_");
                         int height2 = fm2.getHeight();
-                        
+    
                         g2d2.dispose();
-                        
+    
                         img2 = new BufferedImage(width2, height2, BufferedImage.TYPE_INT_ARGB);
-                        
+    
                         g2d2 = img2.createGraphics();
-                        
+    
                         g2d2.setRenderingHint(RenderingHints.KEY_ALPHA_INTERPOLATION, RenderingHints.VALUE_ALPHA_INTERPOLATION_QUALITY);
                         g2d2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
                         g2d2.setRenderingHint(RenderingHints.KEY_COLOR_RENDERING, RenderingHints.VALUE_COLOR_RENDER_QUALITY);
@@ -150,32 +152,32 @@ public class SamQuotesCommand extends Command {
                         g2d2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
                         g2d2.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
                         g2d2.setRenderingHint(RenderingHints.KEY_STROKE_CONTROL, RenderingHints.VALUE_STROKE_PURE);
-                        
+    
                         g2d2.setFont(font);
                         Color nameColor = new Color(45, 102, 212);
                         g2d2.setColor(nameColor);
-                        g2d2.drawString("             SamMan_", 0, fm2.getAscent());
-                        
+                        g2d2.drawString("           SamMan_", 0, fm2.getAscent());
+    
                         g2d2.dispose();
-                        
+    
                         //create background image
-                        
+    
                         BufferedImage combined = new BufferedImage(Math.max(img.getWidth(), img2.getWidth()) + 10, img.getHeight() + 10, BufferedImage.TYPE_INT_ARGB);
-                        
+    
                         Graphics g = combined.getGraphics();
-                        
+    
                         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-                        
+    
                         Color bgColor = new Color(54,57,63);
-                        
-                        BufferedImage bg = new BufferedImage(Math.max(img.getWidth(), img2.getWidth()) + 10, height + 10, BufferedImage.TYPE_INT_ARGB);
+    
+                        BufferedImage bg = new BufferedImage(Math.max(img.getWidth(), img2.getWidth()) + 30, height + 30, BufferedImage.TYPE_INT_ARGB);
                         Graphics2D bgg = bg.createGraphics();
                         bgg.setPaint (bgColor);
                         bgg.fillRect (0, 0, bg.getWidth(), bg.getHeight());
                         bgg.dispose();
-                        
+    
                         //combine images
-                        
+    
                         g.drawImage(bg, 0, 0, bgColor, null);
                         g.drawImage(img, 5, 5, bgColor, null);
                         g.drawImage(img2, 5, 5, bgColor, null);
@@ -254,9 +256,9 @@ public class SamQuotesCommand extends Command {
                 e.printStackTrace();
             }
     
-            String text = "             " + string;
+            String text = "           " + string;
     
-            int pfpWidth = samPfp.getWidth();
+            int pfpWidth = samPfp.getWidth() * 10;
     
             //crop pfp into a circle
     
@@ -275,7 +277,7 @@ public class SamQuotesCommand extends Command {
     
             Graphics2D g2d = img.createGraphics();
     
-            Font font = new Font("Whitney", Font.PLAIN, 14);
+            Font font = new Font("Whitney", Font.PLAIN, 140);
             g2d.setFont(font);
             FontMetrics fm = g2d.getFontMetrics();
     
@@ -309,11 +311,11 @@ public class SamQuotesCommand extends Command {
     
             Graphics2D g2d2 = img2.createGraphics();
     
-            Font font2 = new Font("Whitney", Font.BOLD, 14);
+            Font font2 = new Font("Whitney", Font.BOLD, 140);
             g2d2.setFont(font2);
-            FontMetrics fm2 = g2d.getFontMetrics();
+            FontMetrics fm2 = g2d2.getFontMetrics();
     
-            int width2 = fm2.stringWidth("             SamMan_");
+            int width2 = fm2.stringWidth("           SamMan_");
             int height2 = fm2.getHeight();
     
             g2d2.dispose();
@@ -334,7 +336,7 @@ public class SamQuotesCommand extends Command {
             g2d2.setFont(font);
             Color nameColor = new Color(45, 102, 212);
             g2d2.setColor(nameColor);
-            g2d2.drawString("             SamMan_", 0, fm2.getAscent());
+            g2d2.drawString("           SamMan_", 0, fm2.getAscent());
     
             g2d2.dispose();
     
@@ -348,7 +350,7 @@ public class SamQuotesCommand extends Command {
     
             Color bgColor = new Color(54,57,63);
     
-            BufferedImage bg = new BufferedImage(Math.max(img.getWidth(), img2.getWidth()) + 10, height + 10, BufferedImage.TYPE_INT_ARGB);
+            BufferedImage bg = new BufferedImage(Math.max(img.getWidth(), img2.getWidth()) + 30, height + 30, BufferedImage.TYPE_INT_ARGB);
             Graphics2D bgg = bg.createGraphics();
             bgg.setPaint (bgColor);
             bgg.fillRect (0, 0, bg.getWidth(), bg.getHeight());
