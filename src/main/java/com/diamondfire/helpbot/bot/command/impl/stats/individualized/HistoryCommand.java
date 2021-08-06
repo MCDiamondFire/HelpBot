0d4af9e7d55827a9f8061584394e7f2976424fad
Owen1212055 <23108066+Owen1212055@users.noreply.github.com>
2021-08-05 21:54:40 -0400

    Update HistoryCommand.java

--- src/main/java/com/diamondfire/helpbot/bot/command/impl/stats/individualized/HistoryCommand.java
+++ src/main/java/com/diamondfire/helpbot/bot/command/impl/stats/individualized/HistoryCommand.java
@@ -114,120 +114,72 @@ public class HistoryCommand extends AbstractPlayerUUIDCommand {
                         warningReq = 6 - warnings;
                     }
                     
-                    event.getMember().getUser().openPrivateChannel().queue((privateChannel) -> {
-                        // Retrieve active punishments
-                        {
-                            PresetBuilder activePunishmentsPreset = new PresetBuilder()
-                                    .withPreset(
-                                            new MinecraftUserPreset(playerName, playerUUID),
-                                            new InformativeReply(InformativeReplyType.INFO, "History Recap", null)
-                                    );
-                            EmbedBuilder embed = activePunishmentsPreset.getEmbed();
-                            
-                            List<String> activePunishments = new ArrayList<>();
-                            int warnings = 0;
-                            int yearlyWarnings = 0;
-                            for (Punishment punishment : punishments) {
-                                if (punishment.active) {
-                                    activePunishments.add(punishment.toString());
-                                }
-                                if (punishment.type == PunishmentType.WARN) {
-                                    if (ChronoUnit.DAYS.between(punishment.startTime.toInstant(), Instant.now()) <= 365) {
-                                        yearlyWarnings++;
-                                    }
-                                    warnings++;
-                                }
-                            }
-                            punishments.removeIf((punishment) -> punishment.active);
-                            
-                            if (activePunishments.size() > 0) {
-                                activePunishmentsPreset.withPreset(new InformativeReply(InformativeReplyType.ERROR, "Active Punishments", null));
-                                EmbedUtil.addFields(embed, activePunishments, "", true);
-                                
-                                String duration = null;
-                                int warningReq = 0;
-                                if (warnings < 4 && warnings > 1) {
-                                    duration = "30 minute";
-                                    warningReq = 4 - warnings;
-                                } else if (warnings < 5) {
-                                    duration = "1 hour";
-                                    warningReq = 5 - warnings;
-                                } else if (warnings < 6) {
-                                    duration = "12 hour";
-                                    warningReq = 6 - warnings;
-                                }
-                                
-                                if (duration != null) {
-                                    embed.addField("Tempban", String.format("\u26A0 If you receive **%s** more active %s, you will receive a **%s** tempban!", warningReq, StringUtil.sCheck("warning", warningReq), duration), false);
-                                }
-                                
-                            } else if (punishments.size() == 0) {
-                                embed.setDescription("No punishments here, keep up the good work!");
-                            }
-                            
-                            if (yearlyWarnings > 10) {
-                                embed.setColor(Color.RED);
-                                embed.addField("Tempban", String.format("\u26A0 If you receive **%s** more %s this year, you will receive a **45** day tempban!", 20 - yearlyWarnings, StringUtil.sCheck("warning", 20 - yearlyWarnings)), false);
-                            }
-                            
-                            msgs.add(privateChannel.sendMessageEmbeds(embed.build()));
-                        }
-                        
-                        // Retrieve normal punishments
-                        {
-                            PresetBuilder punishmentsPreset = new PresetBuilder()
-                                    .withPreset(
-                                            new InformativeReply(InformativeReplyType.INFO, "Punishment History", null)
-                                    );
-                            EmbedBuilder presetBuilder = punishmentsPreset.getEmbed();
-                            List<String> punishmentStrings = new ArrayList<>();
-                            for (Punishment punishment : punishments) {
-                                punishmentStrings.add(punishment.toString());
-                            }
-                            
-                            EmbedUtil.addFields(presetBuilder, punishmentStrings, "", "", true);
-                            if (punishmentStrings.size() == 0) {
-                            } else if (presetBuilder.isValidLength()) {
-                                msgs.add(privateChannel.sendMessageEmbeds(presetBuilder.build()));
-                            } else {
-                                try {
-                                    File sendFile = ExternalFileUtil.generateFile("history.txt");
-                                    Files.writeString(sendFile.toPath(), String.join("\n", punishmentStrings));
-                                    msgs.add(privateChannel.sendFile(sendFile));
-                                } catch (IOException exception) {
-                                    exception.printStackTrace();
-                                }
-                            }
-                            
-                        }
-                        
-                        MessageAction action = msgs.get(0);
-                        msgs.remove(0);
-                        
-                        action.queue((msg) -> {
-                            PresetBuilder successMSG = new PresetBuilder()
-                                    .withPreset(
-                                            new InformativeReply(InformativeReplyType.SUCCESS, "Check your messages for history!"),
-                                            new MinecraftUserPreset(playerName, playerUUID)
-                                    );
-                            
-                            event.reply(successMSG);
-                            
-                            for (MessageAction msgAction : msgs) {
-                                msgAction.queue();
-                            }
-                        }, (error) -> {
-                            PresetBuilder errorMSG = new PresetBuilder()
-                                    .withPreset(new InformativeReply(InformativeReplyType.ERROR, "Could not send private message! Please check to make sure private messages are enabled."));
-                            event.reply(errorMSG);
-                        });
-                        
-                    });
-                });
-       
+                    if (duration != null) {
+                        embed.addField("Tempban", String.format("\u26A0 If you receive **%s** more active %s, you will receive a **%s** tempban!", warningReq, StringUtil.sCheck("warning", warningReq), duration), false);
+                    }
+                    
+                } else if (punishments.size() == 0) {
+                    embed.setDescription("No punishments here, keep up the good work!");
+                }
+                
+                if (yearlyWarnings > 10) {
+                    embed.setColor(Color.RED);
+                    embed.addField("Tempban", String.format("\u26A0 If you receive **%s** more %s this year, you will receive a **45** day tempban!", 20 - yearlyWarnings, StringUtil.sCheck("warning", 20 - yearlyWarnings)), false);
+                }
+                
+                msgs.add(privateChannel.sendMessageEmbeds(embed.build()));
+            }
+            
+            // Retrieve normal punishments
+            {
+                PresetBuilder punishmentsPreset = new PresetBuilder()
+                        .withPreset(
+                                new InformativeReply(InformativeReplyType.INFO, "Punishment History", null)
+                        );
+                EmbedBuilder presetBuilder = punishmentsPreset.getEmbed();
+                List<String> punishmentStrings = new ArrayList<>();
+                for (Punishment punishment : punishments) {
+                    punishmentStrings.add(punishment.toString());
+                }
+                
+                EmbedUtil.addFields(presetBuilder, punishmentStrings, "", "", true);
+                if (punishmentStrings.size() == 0) {
+                } else if (presetBuilder.isValidLength()) {
+                    msgs.add(privateChannel.sendMessage(presetBuilder.build()));
+                } else {
+                    try {
+                        File sendFile = ExternalFileUtil.generateFile("history.txt");
+                        Files.writeString(sendFile.toPath(), String.join("\n", punishmentStrings));
+                        msgs.add(privateChannel.sendFile(sendFile));
+                    } catch (IOException exception) {
+                        exception.printStackTrace();
+                    }
+                }
+                
+            }
+            
+            MessageAction action = msgs.get(0);
+            msgs.remove(0);
+            
+            action.queue((msg) -> {
+                PresetBuilder successMSG = new PresetBuilder()
+                        .withPreset(
+                                new InformativeReply(InformativeReplyType.SUCCESS, "Check your messages for history!"),
+                                new MinecraftUserPreset(player)
+                        );
+                
+                event.reply(successMSG);
+                
+                for (MessageAction msgAction : msgs) {
+                    msgAction.queue();
+                }
+            }, (error) -> {
+                PresetBuilder errorMSG = new PresetBuilder()
+                        .withPreset(new InformativeReply(InformativeReplyType.ERROR, "Could not send private message! Please check to make sure private messages are enabled."));
+                event.reply(errorMSG);
+            });
+            
+        });
     }
     
 }
-
-
-
