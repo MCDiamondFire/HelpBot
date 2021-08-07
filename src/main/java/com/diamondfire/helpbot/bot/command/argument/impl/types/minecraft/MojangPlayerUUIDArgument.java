df9d15b8f24eb5ddd4890282c482c71fea318a83
Owen1212055 <23108066+Owen1212055@users.noreply.github.com>
2021-08-07 18:22:34 -0400

    fix

--- src/main/java/com/diamondfire/helpbot/bot/command/argument/impl/types/minecraft/MojangPlayerUUIDArgument.java
+++ src/main/java/com/diamondfire/helpbot/bot/command/argument/impl/types/minecraft/MojangPlayerUUIDArgument.java
@@ -4,8 +4,10 @@ import com.diamondfire.helpbot.bot.HelpBotInstance;
 import com.diamondfire.helpbot.bot.command.argument.impl.parsing.exceptions.ArgumentException;
 import com.diamondfire.helpbot.bot.command.argument.impl.types.AbstractSimpleValueArgument;
 import com.diamondfire.helpbot.bot.events.CommandEvent;
-import com.google.gson.*;
-import okhttp3.*;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import okhttp3.Request;
+import okhttp3.ResponseBody;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
@@ -15,8 +17,12 @@ public class MojangPlayerUUIDArgument extends AbstractSimpleValueArgument<UUID> 
     
     @Override
     protected UUID parse(@NotNull String argument, CommandEvent event) throws ArgumentException {
-        if (argument.contains("-") || argument.length() > 16) {
-            return UUID.fromString(argument);
+        if (argument.length() > 16) {
+            if (argument.contains("-")) {
+                return UUID.fromString(argument);
+            } else {
+                return UUID.fromString(argument.replaceFirst("(\\p{XDigit}{8})(\\p{XDigit}{4})(\\p{XDigit}{4})(\\p{XDigit}{4})(\\p{XDigit}+)", "$1-$2-$3-$4-$5"));
+            }
         } else {
             JsonObject responseObject = null;
             ResponseBody res = null;
@@ -28,7 +34,7 @@ public class MojangPlayerUUIDArgument extends AbstractSimpleValueArgument<UUID> 
             }
             
             try {
-                 responseObject = JsonParser.parseString(res.string()).getAsJsonObject();
+                responseObject = JsonParser.parseString(res.string()).getAsJsonObject();
             } catch (IOException e) {
                 e.printStackTrace();
             }
