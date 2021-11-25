292fd6320db7e9185f4a16db5e4623da864212ca
tk2217 <52385653+tk2217@users.noreply.github.com>
2021-11-25 12:58:08 -0600

    admin disabled command bypass + move to a Set

--- src/main/java/com/diamondfire/helpbot/bot/command/executor/checks/CommandCheck.java
+++ src/main/java/com/diamondfire/helpbot/bot/command/executor/checks/CommandCheck.java
@@ -6,6 +6,11 @@ import com.diamondfire.helpbot.bot.events.command.CommandEvent;
 // Command checks are run before a command is executed.
 public interface CommandCheck {
     
+    /**
+     * Checks whether a command event should be run.
+     * @param event The command event to check.
+     * @return Whether the command should be run.
+     */
     boolean check(CommandEvent event);
     
     void buildMessage(CommandEvent event, PresetBuilder builder);
--- src/main/java/com/diamondfire/helpbot/bot/command/executor/checks/DisabledCheck.java
+++ src/main/java/com/diamondfire/helpbot/bot/command/executor/checks/DisabledCheck.java
@@ -1,6 +1,7 @@
 package com.diamondfire.helpbot.bot.command.executor.checks;
 
 import com.diamondfire.helpbot.bot.command.CommandHandler;
+import com.diamondfire.helpbot.bot.command.permissions.Permission;
 import com.diamondfire.helpbot.bot.command.reply.PresetBuilder;
 import com.diamondfire.helpbot.bot.command.reply.feature.informative.*;
 import com.diamondfire.helpbot.bot.events.command.CommandEvent;
@@ -8,7 +9,8 @@ import com.diamondfire.helpbot.bot.events.command.CommandEvent;
 public class DisabledCheck implements CommandCheck {
     @Override
     public boolean check(CommandEvent event) {
-        return !CommandHandler.getInstance().getDisabledHandler().isDisabled(event.getCommand());
+        return Permission.ADMINISTRATOR.hasPermission(event.getMember())
+                || !CommandHandler.getInstance().getDisabledHandler().isDisabled(event.getCommand());
     }
     
     @Override
