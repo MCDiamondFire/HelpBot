17cae2e50227c537cae51ef878d3f7e5131ec3b2
Reasonless <28310208+Reasonlesss@users.noreply.github.com>
2022-06-13 19:31:43 +0100

    forgor :skull:

--- src/main/java/com/diamondfire/helpbot/bot/command/impl/other/fun/CowsayCommand.java
+++ src/main/java/com/diamondfire/helpbot/bot/command/impl/other/fun/CowsayCommand.java
@@ -6,6 +6,7 @@ import com.diamondfire.helpbot.bot.command.help.*;
 import com.diamondfire.helpbot.bot.command.impl.Command;
 import com.diamondfire.helpbot.bot.command.permissions.Permission;
 import com.diamondfire.helpbot.bot.events.CommandEvent;
+import com.diamondfire.helpbot.util.StringUtil;
 import net.dv8tion.jda.api.EmbedBuilder;
 
 public class CowsayCommand extends Command {
