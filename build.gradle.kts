b003593aefa6635e19cf519c49f3d66d8063f889
Daedalus <16168171+RedDaedalus@users.noreply.github.com>
2022-10-01 12:17:21 -0600

    chore: Move to gradle Kotlin DSL (groovy sucks nobody likes it) (#88)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,54 @@
 plugins {
+    java
+    application
+    id("com.github.johnrengelman.shadow") version "7.1.2"
+}
+
+application {
+    mainClass.set("com.diamondfire.helpbot.HelpBot")
+}
+
+group = "com.diamondfire.helpbot"
+
+repositories {
+    mavenCentral()
+    maven(url = "https://jitpack.io")
+    maven(url = "https://m2.dv8tion.net/releases")
+}
+
+dependencies {
+    implementation("net.dv8tion:JDA:4.3.0_305") {
+        exclude(module = "opus-java")
+    }
+
+    implementation("club.minnced:discord-webhooks:0.5.8")
+    implementation("ch.qos.logback:logback-classic:1.2.5")
+    implementation("com.google.code.gson:gson:2.8.6")
+    implementation("com.github.Steveice10:MCProtocolLib:c5e4b66")
+    implementation("mysql:mysql-connector-java:5.1.13")
+    implementation("org.codehaus.groovy:groovy-jsr223:3.0.8")
+}
+
+tasks {
+    compileJava {
+        options.encoding = "UTF-8"
+    }
+
+    jar {
+        manifest {
+            attributes["Main-Class"] = "com.diamondfire.helpbot.HelpBot"
+        }
+    }
+
+    build {
+        dependsOn(shadowJar)
+    }
+}
+
+
+
+/*
+plugins {
     id 'java'
     id 'application'
     id 'com.github.johnrengelman.shadow' version '7.0.0'
@@ -43,3 +93,5 @@ build {
         shadowJar
     }
 }
+
+ */
