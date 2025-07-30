53963a52d95bf0dc395fdc7dbb729a783cda1f1c
RedVortexDev <58099979+RedVortexDev@users.noreply.github.com>
2025-07-31 01:13:35 +0300

    fix: non-verified users are able to run user commands

--- src/main/java/com/diamondfire/helpbot/bot/command/impl/stats/AbstractPlayerUUIDCommand.java
+++ src/main/java/com/diamondfire/helpbot/bot/command/impl/stats/AbstractPlayerUUIDCommand.java
@@ -5,7 +5,10 @@ import com.diamondfire.helpbot.bot.command.argument.impl.parsing.types.SingleArg
 import com.diamondfire.helpbot.bot.command.argument.impl.types.minecraft.*;
 import com.diamondfire.helpbot.bot.command.impl.Command;
 import com.diamondfire.helpbot.bot.command.permissions.Permission;
+import com.diamondfire.helpbot.bot.command.reply.PresetBuilder;
+import com.diamondfire.helpbot.bot.command.reply.feature.informative.*;
 import com.diamondfire.helpbot.bot.events.CommandEvent;
+import com.diamondfire.helpbot.util.Util;
 
 public abstract class AbstractPlayerUUIDCommand extends Command {
     
@@ -25,6 +28,18 @@ public abstract class AbstractPlayerUUIDCommand extends Command {
     public void run(CommandEvent event) {
         Player player = event.getArgument("player");
         if (player == null) {
+            // If no player is specified, use the member's name,
+            // however, non-verified members can set their display name
+            // to any player and run commands as that player; therefore,
+            // we need to check if the member is verified.
+            if (!event.getMember().getRoles().contains(event.getGuild().getRoleById(Util.VERIFIED))) {
+                PresetBuilder preset = new PresetBuilder()
+                        .withPreset(
+                                new InformativeReply(InformativeReplyType.ERROR, "You must be verified to use this command!")
+                        );
+                event.reply(preset);
+                return;
+            }
             execute(event, DFPlayerArgument.fetchPlayer(event.getMember().getEffectiveName()));
         } else {
             execute(event, player);
